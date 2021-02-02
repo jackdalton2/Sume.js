bf9582bc2b0bddac3e46de02071ba127e3f5c1fc
Jack Dalton <jack@jackdalton.org>
2021-02-02 07:53:36 -0700

    Fix computation of length difference

--- src/compiler.js
+++ src/compiler.js
@@ -5,13 +5,14 @@ module.exports = function(template, blocks, context) {
 
     let length = blocks.length;
     let stop = blocks.length - 1;
+    let totalLenDifference = 0;
 
     for (let i = 0; i < length; i++) {
         let path = blocks[i].flag.split(".");
         let val = context;
 
-        for (let i in path) {
-            val = val[path[i]];
+        for (let j in path) {
+            val = val[path[j]];
         }
 
         val = val.replace(/\&/g, '&amp;')
@@ -19,12 +20,12 @@ module.exports = function(template, blocks, context) {
             .replace(/\>/g, '&gt;')
             .replace(/\"/g, '&quot;')
             .replace(/\'/g, '&#x27');
-        
+
         if (i < stop) {
-            let lenDifference = val.length - (4 + blocks[i].flag.length) - blocks[i].stripped;
+            totalLenDifference += val.length - 4 - blocks[i].flag.length - blocks[i].stripped;
             let next = i + 1;
-            blocks[next].indices[0] += lenDifference;
-            blocks[next].indices[1] += lenDifference;
+            blocks[next].indices[0] += totalLenDifference;
+            blocks[next].indices[1] += totalLenDifference;
         }
 
         template = template.substring(0, blocks[i].indices[0]) + val + template.substring(blocks[i].indices[1]);
